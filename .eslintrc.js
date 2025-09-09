fb25a1650f3300eb0ac23cdda5448299ca19862c
ikhlaq1 <ikhlaq201@gmail.com>
2025-09-09 19:17:59 +0530

    Added initial dev packages for project setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,59 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+
+  parser: '@typescript-eslint/parser',
+
+  parserOptions: {
+    ecmaVersion: 2021,
+
+    sourceType: 'module',
+
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  plugins: [
+    '@typescript-eslint',
+
+    'react',
+
+    'react-hooks',
+
+    'react-native',
+
+    'prettier',
+  ],
+
+  extends: [
+    'eslint:recommended',
+
+    'plugin:@typescript-eslint/recommended',
+
+    'plugin:react/recommended',
+
+    'plugin:react-hooks/recommended',
+
+    'plugin:react-native/all',
+
+    'prettier',
+  ],
+
+  rules: {
+    'prettier/prettier': 'error',
+
+    'react/prop-types': 'off',
+
+    'react-native/no-inline-styles': 'warn',
+
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
