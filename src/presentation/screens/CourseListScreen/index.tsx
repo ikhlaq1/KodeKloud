fbb21ff5fa0baf1b8e77de08e040b57234eade00
ikhlaq1 <ikhlaq201@gmail.com>
2025-09-11 14:39:20 +0530

    created custom hook and omoved all business logic into that

--- src/presentation/screens/CourseListScreen/index.tsx
+++ src/presentation/screens/CourseListScreen/index.tsx
@@ -1,65 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { View, FlatList, ActivityIndicator, Text } from 'react-native';
 import CourseCard from '../../components/CourseCard';
 import { styles } from './styles';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../../../store/store';
-import {
-  appendCourses,
-  setCourses,
-  setCurrentPage,
-  setError,
-  setHasMore,
-  setLoading,
-  setLoadingMore,
-} from '../../../store/courseSlice';
 import { useNavigation } from '@react-navigation/native';
 import { NavigationProp } from '../../../navigation/types';
-import { useCourseUseCases } from '../../../context/DependencyContext';
+import { useCourseList } from '../../hooks/useCourseList';
 
 const CourseListScreen = () => {
   const navigation = useNavigation<NavigationProp>();
-  const dispatch = useDispatch<AppDispatch>();
-  const courseUseCases = useCourseUseCases();
-  //replaced all state variables with values in store
-  const { courses, currentPage, hasMore, loading, loadingMore, error } = useSelector(
-    (state: RootState) => state.courses,
-  );
-
-  useEffect(() => {
-    fetchCourses();
-  }, []);
-
-  const fetchCourses = async () => {
-    try {
-      dispatch(setLoading(true));
-      const response = await courseUseCases.getCourses(1);
-
-      dispatch(setCourses(response.courses));
-      dispatch(setCurrentPage(1));
-      dispatch(setHasMore(response.metadata.next_page !== null));
-    } catch (err) {
-      dispatch(setError('Failed to load courses'));
-      console.error(err);
-    }
-  };
-
-  const loadMore = async () => {
-    if (loadingMore || !hasMore) return;
-
-    setLoadingMore(true);
-    try {
-      const nextPage = currentPage + 1;
-      const response = await courseUseCases.loadMoreCourses(nextPage, courses);
-      dispatch(appendCourses(response.allCourses.slice(courses.length)));
-      dispatch(setCurrentPage(nextPage));
-      dispatch(setHasMore(response.hasMore));
-    } catch (err) {
-      console.error('Failed to load more:', err);
-      dispatch(setLoadingMore(false));
-    }
-    setLoadingMore(false);
-  };
+  const { courses, loading, loadingMore, error, loadMore } = useCourseList();
 
   const handleCoursePress = (course: any) => {
     navigation.navigate('CourseDetail', {
